bda28bcf6e0ac4f18d773922788ae5d7a907cc21
toudatouda <lhsokgood@163.com>
2022-08-12 16:14:01 +0800

    Add files via upload

---
+++ sql_left_join_pq1_count_rows.sql
@@ -0,0 +1,7 @@
+SELECT
+	COUNT( companies.permalink ) AS count_companies,
+	COUNT( acquisitions.company_permalink ) AS count_acquisitions 
+FROM
+	tutorial.crunchbase_companies companies
+JOIN 
+	tutorial.crunchbase_acquisitions acquisitions ON companies.permalink = acquisitions.company_permalink
